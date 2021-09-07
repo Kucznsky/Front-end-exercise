8f80bc513812959101c1979ec21ad2fd95bc90d4
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-09-07 15:59:29 +0200

    logic added

--- JS/interval/Interval.js
+++ JS/interval/Interval.js
@@ -2,4 +2,8 @@ function CreateInterval()
 {
     var number1 = document.getElementById(first).value;
     var number2 = document.getElementById(second).value;
-}
+    var numbers = number1;
+    for(let i=number1+1;i==number2;i++)
+        numbers=numbers+", "+i;
+    document.getElementById(intervalOfNumbers).innerHTML=numbers;
+} 
