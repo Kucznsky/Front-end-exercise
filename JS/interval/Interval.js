e9845b4e7d0650f8e4e55d9afb35209a791707b5
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-09-08 15:55:16 +0200

    it works

--- JS/interval/Interval.js
+++ JS/interval/Interval.js
@@ -1,9 +1,9 @@
 function CreateInterval()
 {
-    var number1 = document.getElementById(first).value;
-    var number2 = document.getElementById(second).value;
-    var numbers = number1;
-    for(let i=number1+1;i==number2;i++)
-        numbers=numbers+", "+i;
-    document.getElementById(intervalOfNumbers).innerHTML=numbers;
+    var number1 = document.getElementById("first").value;
+    var number2 = document.getElementById("second").value;
+    var numbers = ""
+    for(i=number1;i<=number2;i++)
+        numbers=numbers + i + ", ";
+    document.getElementById("intervalOfNumbers" ).innerHTML=numbers;
 } 
