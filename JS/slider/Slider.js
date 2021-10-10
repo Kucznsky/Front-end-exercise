0c0c18c22bd6a6a778f9576ad34cf7f05d22e270
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-10-10 21:03:04 +0200

    slider works

--- JS/slider/Slider.js
+++ JS/slider/Slider.js
@@ -1,1 +1,13 @@
-//work in progress
+var number = Math.floor(Math.random()*7)+1; //floor zaokragla liczbe w dol//losowanie liczby od 1 do 7
+
+function ChangeSlide()
+{
+    number++;
+    if(number>7)
+        number=1;
+    var file = "<img src=\"Data//s" + number + ".png\">";
+
+    document.getElementById("slider").innerHTML = file;
+
+    setTimeout("ChangeSlide()",10000) //funkcja ma sie wywolac po uplywie 10 sekund
+}
