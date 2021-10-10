4688ff15d09c3314772f12568162201c9b23d3c2
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-10-10 23:20:27 +0200

    font awsome icons added in css, project finished

--- JS/slider/Slider.js
+++ JS/slider/Slider.js
@@ -1,5 +1,8 @@
 var number = Math.floor(Math.random()*7)+1; //floor zaokragla liczbe w dol//losowanie liczby od 1 do 7
 
+var timer1;
+var timer2;
+
 function Hide()
 {
     $("#slider").fadeOut(500); //animacja znikniecia slajdow
@@ -13,7 +16,17 @@ function ChangeSlide()
 
     document.getElementById("slider").innerHTML = file;
     $("#slider").fadeIn(500); //animacja pojawienia sie slajdow
-    setTimeout("ChangeSlide()",5000) //funkcja ma sie wywolac po uplywie 10 sekund
-    setTimeout("Hide()",4500);
+    timer1 = setTimeout("ChangeSlide()",5000) //funkcja ma sie wywolac po uplywie 10 sekund
+    timer2 = setTimeout("Hide()",4500);
     
+}
+function SetSlide(nr)
+{
+    //wyzerowanie licznika zmiany slajdow 
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    //-----------------------------------
+    numer=nr - 1;
+    Hide();
+    setTimeout(ChangeSlide(),5000);
 }
